1efd5bd1022586e6e60bd2ffe861e068e1f80db0
richardbatstone <36573392+richardbatstone@users.noreply.github.com>
2020-11-17 21:15:51 +0000

    Add socket detail

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 """Flask App Project."""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json, request
+from flask_cors import CORS
+
+sockets = {
+    'socket_1': False,
+    'socket_2': False
+}
+
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
-def index():
-    """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
+def hello():
+	return "<h1>Pimote api</h1>"
 
+@app.route('/sockets/', methods=['POST'])
+def set_socket_values():
+    if not request.json:
+        return jsonify({'error':'error 1, send json'}), 400
+    if 'socket_1' and 'socket_2' in request.json:
+        if type(request.json['socket_1']) == bool and type(request.json['socket_2']) == bool:
+            sockets['socket_1'] = request.json['socket_1']
+            sockets['socket_2'] = request.json['socket_2']
+            return jsonify({'message': 'sockets set'}), 201
+        else:
+            return jsonify({'error':'set booleans'}), 400
+    else:
+        return jsonify({'error':'set sockets'}), 400
+
+@app.route('/sockets/', methods=['GET'])
+def get_socket_values():
+    return jsonify(sockets)
 
 if __name__ == '__main__':
     app.run()
